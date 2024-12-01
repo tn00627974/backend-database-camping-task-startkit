51af1b4ad25cbba7eafdeb10a7030273773dd2c9
Ulysses <tn00627974@gmail.com>
2024-12-01 11:10:09 +0800

    TASK 2 提交答案

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -1,4 +1,3 @@
-
 -- ████████  █████   █     █ 
 --   █ █   ██    █  █     ██ 
 --   █ █████ ███ ███       █ 
@@ -56,10 +55,40 @@ limit 3 ;
     -- 2. 名稱為`14 堂組合包方案`，價格為`2,520` 元，堂數為`14`
     -- 3. 名稱為 `21 堂組合包方案`，價格為`4,800` 元，堂數為`21`
 
+INSERT INTO "CREDIT_PACKAGE" (name, credit_amount,price) values
+('7 堂組合包方案',7,1400),
+('14 堂組合包方案',14,2520),
+('21 堂組合包方案',21,4800);
+
 -- 2-2. 新增：在 `CREDIT_PURCHASE` 資料表，新增三筆資料：（請使用 name 欄位做子查詢）
+
     -- 1. `王小明` 購買 `14 堂組合包方案`
+INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id, purchased_credits, price_paid) values
+(
+	(SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io'), 
+	(SELECT id FROM "CREDIT_PACKAGE"  WHERE name = '14 堂組合包方案'),
+	(SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案' ),
+	(SELECT price FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案' )
+);
+
     -- 2. `王小明` 購買 `21 堂組合包方案`
+INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id, purchased_credits, price_paid) values
+(
+	(SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io'), 
+	(SELECT id FROM "CREDIT_PACKAGE"  WHERE name = '21 堂組合包方案'),
+	(SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案' ),
+	(SELECT price FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案' )
+);
+
     -- 3. `好野人` 購買 `14 堂組合包方案`
+INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id, purchased_credits, price_paid) values
+(
+	(SELECT id FROM "USER" WHERE email = 'richman@hexschooltest.io'), 
+	(SELECT id FROM "CREDIT_PACKAGE"  WHERE name = '14 堂組合包方案'),
+	(SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案' ),
+	(SELECT price FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案' )
+);
+
 
 
 -- ████████  █████   █    ████   
